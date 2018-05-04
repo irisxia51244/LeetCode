2f50ef0fd7176f9b92ae4b7eeda8ba1e84dae944
irisxia51244 <xias51244@gmail.com>
2018-05-03 22:18:06 -0700

    16，【Medium】，【Array】【Two Pointers】

--- serial_kSum/ThreeSum_Closest_16.java
+++ serial_kSum/ThreeSum_Closest_16.java
@@ -1,30 +1,33 @@
 import java.util.*;
 /**
- * 259，【Medium】，【Array】【Two Pointers】
- * LeetCode 259. 3Sum Smaller
+ * 16，【Medium】，【Array】【Two Pointers】
+ * LeetCode 16.  3Sum Closest
  * Related Topics: Array, Two Pointers
  * @author Iris Xia
  * Time Complexity: O(n^2), Space Complexity: O(1);
  */
 
-//nums[i] + nums[l]+ nums[r] < target => nums[i] + nums[l] + nums[from l+1 to r] < target
 class Solution {
-    public int threeSumSmaller(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return 0;
-        int res = 0;
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums == null || nums.length < 3) return 0;
         int n = nums.length;
+        int res = nums[0] + nums[1] + nums[n-1];
         Arrays.sort(nums);
         for(int i = 0; i < n-2; i++){
             int l = i+1, r = n-1;
             while(l < r){
-                int sum = nums[i] + nums[l] + nums[r]; 
-                if(sum < target){
-                    res += r - l;
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == target) return sum;
+                else if(sum < target){
                     l++;
-                }else{
+                }
+                else{
                     r--;
                 }
-            } 
+                if(Math.abs(sum - target) < Math.abs(res - target)){
+                    res = sum;
+                }
+            }
         }
         return res;
     }

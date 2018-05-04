6c63b573d8073a02a9e6cd2a785f8814c31337f4
irisxia51244 <xias51244@gmail.com>
2018-05-04 02:01:50 -0700

    121，【Easy】，【Array】【DP】

--- serial_stock/BestTimeToBuyAndSellStock.java
+++ serial_stock/BestTimeToBuyAndSellStock.java
@@ -1,1 +1,25 @@
+import java.util.*;
+/**
+ * 121，【Easy】，【Array】【DP】
+ * LeetCode 121. Best Time to Buy and Sell Stock
+ * Related Topics: Array, Dynamic Programming
+ * @author Iris Xia
+ * Time Complexity: O(n), Space Complexity: O(1);
+ */
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        
+        for(int p: prices){
+            if(p < minPrice){
+                minPrice = p;
+            }else{
+                maxProfit = Math.max(maxProfit, p - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}

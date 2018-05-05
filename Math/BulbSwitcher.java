133f5615b8670abb2c8aa4cdcbb0c6a29f76dea0
irisxia51244 <xias51244@gmail.com>
2018-05-04 23:28:43 -0700

    319，【Medium】，【Math】【Brainteaser】

---
+++ Math/BulbSwitcher.java
@@ -0,0 +1,14 @@
+import java.util.*;
+/**
+ * 319，【Medium】，【Math】【Brainteaser】
+ * LeetCode 
+ * Related Topics: 
+ * @author Iris Xia
+ * Time Complexity: O(1), Space Complexity: O(1);
+ */
+ 
+ class Solution {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}

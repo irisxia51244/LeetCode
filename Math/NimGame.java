eff7d45bc426546b2e3023c43bcc0d8545d56be2
irisxia51244 <xias51244@gmail.com>
2018-05-06 16:03:14 -0700

    292，【Easy】，【Brainteaser】

--- Math/NimGame.java
+++ Math/NimGame.java
@@ -3,7 +3,7 @@ import java.util.*;
  * 292，【Easy】，【Brainteaser】
  * LeetCode 292. Nim Game
  * Related Topics: Brainteaser
- * @author Iris Xia
+ * @author Iris Xia.
  * Time Complexity: O(1), Space Complexity: O(1);
  */
 

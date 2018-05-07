6a481e64b6dc5d0fabc4342c3fac1177e1b4df53
irisxia51244 <xias51244@gmail.com>
2018-05-07 00:12:42 -0700

    242，【Easy】，【HashTable】【Sort】

---
+++ String/Serial_Anagram/ValidAnagram.java
@@ -0,0 +1,27 @@
+/**
+ * 242，【Easy】，【HashTable】【Sort】
+ * LeetCode 242. Valid Anagram
+ * Related Topics: HashTable, Sort
+ * @author Iris Xia
+ * Time Complexity: O(n), Space Complexity: O(1);
+ */
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < count.length; i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

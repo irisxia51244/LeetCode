4cb6de9ec61993ba6ab48f336abf3b66b9ad720c
irisxia51244 <xias51244@gmail.com>
2018-05-07 01:53:46 -0700

    84，【Hard】，【Array】【Stack】

---
+++ ScanLine/LargestRectangleinHistogram.java
@@ -0,0 +1,26 @@
+/**
+ * 84，【Hard】，【Array】【Stack】
+ * LeetCode 84. Largest Rectangle in Histogram
+ * Related Topics: Array, Stack
+ * @author Iris Xia
+ * Time Complexity: O(), Space Complexity: O();
+ */
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for(int i = 0; i <= heights.length; i++){
+            int h = i == heights.length? 0: heights[i];
+            while(!stack.isEmpty() && h < heights[stack.peek()]){
+                int height = heights[stack.pop()];
+                int index = stack.isEmpty()?-1:stack.peek();
+                int area = height * (i - index - 1);
+                max = Math.max(max, area);
+            }
+            stack.push(i);
+        }
+        return max;
+    }
+}

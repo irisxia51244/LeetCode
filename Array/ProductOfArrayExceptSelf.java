1c09c7fa712f2e8f1a7cde0648b8e6f8efd6cd0f
irisxia51244 <xias51244@gmail.com>
2018-05-08 00:40:20 -0700

    238，【Medium】，【Array】

---
+++ Array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,24 @@
+/**
+ * 238，【Medium】，【Array】
+ * LeetCode 238. Product of Array Except Self
+ * Related Topics: Array
+ * @author Iris Xia
+ * Time Complexity: O(), Space Complexity: O();
+ */
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for(int i = 1; i < nums.length; i++){
+            res[i] = res[i-1] * nums[i-1];
+        }
+        
+        int right_product = 1; 
+        for(int i = nums.length-1; i >= 0; i--){
+            res[i] *= right_product;
+            right_product *= nums[i];
+        }
+        return res;
+    }
+}

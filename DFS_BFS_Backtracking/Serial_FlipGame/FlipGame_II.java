dfec79888fd66f8d39b095c901f9a7c6efb53047
irisxia51244 <xias51244@gmail.com>
2018-05-08 18:11:02 -0700

    294，【Medium】，【Backtracking】

---
+++ DFS_BFS_Backtracking/Serial_FlipGame/FlipGame_II.java
@@ -0,0 +1,34 @@
+/**
+ * 294，【Medium】，【Backtracking】
+ * LeetCode 294. Flip Game II
+ * Related Topics: Backtracking
+ * @author Iris Xia
+ * Time Complexity: O(), Space Complexity: O();
+ */
+
+class Solution {
+    public boolean canWin(String s) {
+        if(s == null || s.length() == 0){
+            return false;
+        }
+        HashMap<String, Boolean> map = new HashMap<>();
+        return helper(s, map);
+    }
+    
+    private boolean helper(String s, HashMap<String, Boolean> map){
+        if(map.containsKey(s)){
+            return map.get(s);
+        }
+        for(int i = 0; i < s.length()-1; i++){
+            if(s.charAt(i) == '+' && s.charAt(i+1) == '+'){
+                String temp = s.substring(0,i) + "--" + s.substring(i+2);
+                if(!helper(temp, map)){
+                    map.put(s, true);
+                    return true;
+                }
+            }
+        }
+        map.put(s, false);
+        return false;
+    }
+}

d4378cd4f64a9d1f2049ea51ad2459e9ca7c39d2
irisxia51244 <xias51244@gmail.com>
2018-05-08 17:58:01 -0700

    282，【Medium】，【Divide&Conquer】

---
+++ DFS_BFS_Backtracking/ExpressionAddOperators.java
@@ -0,0 +1,36 @@
+/**
+ * 282，【Medium】，【Divide&Conquer】
+ * LeetCode 282. Expression Add Operators
+ * Related Topics: 
+ * @author Iris Xia
+ * Time Complexity: O(), Space Complexity: O();
+ */
+
+class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        if(num == null || num.length()==0) return res;
+        helper(res, num, "", target, 0, 0, 0);
+        return res;
+    }
+    
+    private void helper(List<String> res, String num, String path, int target, 
+                        int pos, long val, long pre){
+        if(pos == num.length()){
+            if(val == target){
+                res.add(path);
+            }
+        }
+        for(int i = pos; i < num.length(); i++){
+            if(i!=pos && num.charAt(pos) == '0') break;
+            long cur = Long.parseLong(num.substring(pos, i+1));
+            if(pos == 0){
+                helper(res, num, path+cur, target, i+1, cur, cur);
+            }else{
+                helper(res, num, path+"+"+cur, target, i+1, val+cur, cur);
+                helper(res, num, path+"-"+cur, target, i+1, val-cur, -cur);
+                helper(res, num, path+"*"+cur, target, i+1, val-pre + pre*cur, pre*cur);
+            }
+        }  
+    }
+}

b66e4b667540c4d99597f475ce737428acd37f04
irisxia51244 <xias51244@gmail.com>
2018-05-08 19:45:20 -0700

    334，【Medium】

---
+++ Array/IncreasingTripletSubsequence.java
@@ -0,0 +1,22 @@
+/**
+ * 334，【Medium】
+ * LeetCode 334. Increasing Triplet Subsequence
+ * Related Topics: 
+ * @author Iris Xia
+ * Time Complexity: O(), Space Complexity: O();
+ */
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums == null || nums.length < 3) return false;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for(int num: nums){
+            if(num <= min1) min1 = num;
+            else if(num <= min2) min2 = num;
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}

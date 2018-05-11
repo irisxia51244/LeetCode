5749c18610af91692b8e424ae3264513476814ce
irisxia51244 <xias51244@gmail.com>
2018-05-10 22:33:05 -0700

    ...

---
+++ BitManipulation/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,7 @@
+/**
+ * 693，【Easy】，【Bit Manipulation】
+ * LeetCode 693. Binary Number with Alternating Bits
+ * Related Topics: Bit Manipulation
+ * @author Iris Xia
+ * Time Complexity: O(), Space Complexity: O();
+ */

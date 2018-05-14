6f78270ed3de196f01ac44e0a5eee6105db5b271
irisxia51244 <xias51244@gmail.com>
2018-05-13 18:22:09 -0700

    336，【Hard】，【Hashtable】【String】【Trie】

--- #Series/Series_Palindrome/PalindromePairs.java
+++ #Series/Series_Palindrome/PalindromePairs.java
@@ -3,10 +3,21 @@
  * LeetCode 336. Palindrome Pairs
  * Related Topics: HashTable, String, Trie
  * @author Iris Xia
- * Time Complexity: O(), Space Complexity: O();
+ * Time Complexity: O(nm^2), Space Complexity: O(n);
  */
 
 class Solution {
+    /*
+     HashMap存所有string和对应index
+     把所有word和其index加入hashmap中
+     如果有“”，加所有palidrome(因为遍历中中，长度为0的string无法分割为两半处理，eg, 遍历中会产生[a,""]，不会产生["", a])
+     遍历所有word：
+        将word中的每一位设为分割点，分割成两个substring
+        如果substring1是回文，找substring2的reverse，如果有，加入[2_reverse,1]
+        如果substring2是回文，找substring1的reverse，如果有，加入[2,1_reverse]
+        *注意找到的reverse不能是substring自己
+        *注意word本身只能被处理一次，所以判断substring2时需要判断是否长度为0
+     */
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> res = new ArrayList<>();
         if(words == null || words.length < 2) return res;
